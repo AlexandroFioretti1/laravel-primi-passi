325c7a61d209ed8fdd1157308b57ac2ba48eefb3
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-05-22 15:50:33 +0200

    add dinamic main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>laravel-primi-passi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
 
-<h1>hello world</h1>
+    <header class="text-center my-5 bg-dark text-light">
+        <h1>Welocome to Valorant</h1>
+    </header>
+
+    <main>
+        <div class="container text-center">
+            <div class="card-header">
+                <h2>{{$gioco}}</h2>
+            </div>
+            <div class="card-body">
+                <span>{{$tipo}}</span>
+                <span>{{$limiteEta}}</span>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container text-center mt-5">
+            <span class="">Copy by &copy; Alexandro Fioretti</span>
+        </div>
+    </footer>
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'gioco' => 'Valorant',
+        'tipo' => 'Fps',
+        'limiteEta' => '18+',
+    ];
+    return view('home', $data);
 });
